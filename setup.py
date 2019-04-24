f6b2e951ce6c9ee07c6bdae9948d4483ddd4408a
bellockk <bellockk@segmail.com>
2019-04-24 13:48:14 -0400

    Ticking version number

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bellockk/alphashape',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
