a2d2f76775218ec064be02724694ac232e7b522a
bellockk <bellockk@segmail.com>
2019-04-24 13:10:16 -0400

    Including shapely dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0',
+                'shapely>=1.4.0']
 
 setup_requirements = [ ]
 
