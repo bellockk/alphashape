b17abdc40b2da917ebe98daaa63b539c4fcfbb70
bellockk <bellockk@segmail.com>
2019-04-25 12:48:47 -0400

    Mocking out dependencies during documentation builds

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,23 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
 import alphashape
+
+MOCK_MODULES = ['shapely']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- General configuration ---------------------------------------------
 
