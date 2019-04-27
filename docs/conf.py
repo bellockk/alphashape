0f512edf28847ecdb3856d13db924870a11a7984
bellockk <bellockk@segmail.com>
2019-04-27 06:22:52 -0400

    Debugging markdown inclusion in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ from _version import __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'm2r',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
