667cec0b87206268b73f6c364c6cd3d941d5ad93
bellockk <bellockk@segmail.com>
2019-05-05 18:12:45 -0400

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bellockk/alphashape',
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
 )
