377b87cbc3d8291da183519cf21ae7d314d462a6
bellockk <bellockk@segmail.com>
2019-05-05 20:26:35 -0400

    Fixing syntax error

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='alphashape',
     name='alphashape',
