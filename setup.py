82776c1db78f5b62322f57d645a4161cda05dc18
Kenneth E. Bellock <ken@bellock.net>
2021-02-25 17:41:27 -0500

    Removing python 3.6 support

--- setup.py
+++ setup.py
@@ -35,10 +35,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
     ],
     description="Toolbox for generating alpha shapes.",
     entry_points={
