d9b888af45754595a775cfddea4aa565f29ad417
Kenneth E. Bellock <ken@bellock.net>
2021-02-25 16:55:07 -0500

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bellockk/alphashape',
-    version='1.1.0',
+    version='1.2.0',
     zip_safe=False,
 )
